40c1b9b0b5fa671d46ef3bc114bd0a54558640fe
GIRIVASAN M <radhikagirivasan@gmail.com>
2025-12-18 07:38:09 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ private_rooms = {}
 
 from extensions import db
 db.init_app(app)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='Threading')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
@@ -651,6 +651,7 @@ app.register_blueprint(call_bp)
 if __name__ == "__main__":
     print("🚀 Server running at: http://127.0.0.1:5000")
     socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+
 
 
 
